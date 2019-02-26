7586f9fddc59fbd040601f5cc6c2143bca7b14b2
Aryaman Gupta <aryaman1994@gmail.com>
2019-02-26 11:50:15 +0100

    Initial Commit

---
+++ src/main/kotlin/graphics/scenery/insitu/SceneryParallelBase.kt
@@ -0,0 +1,7 @@
+package graphics.scenery.insitu
+
+import graphics.scenery.SceneryBase
+
+class SceneryParallelBase : SceneryBase("ParallelBase") {
+
+}
---
+++ src/test/kotlin/graphics/scenery/insitu/MpiExample.kt
@@ -0,0 +1,16 @@
+package graphics.scenery.insitu
+
+import graphics.scenery.SceneryBase
+import org.junit.Test
+
+class MpiExample: SceneryBase ("MPIExample") {
+    override fun init() {
+        super.init()
+
+    }
+
+    @Test
+    override fun main() {
+        super.main()
+    }
+}
