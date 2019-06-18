c945bedfd48fe9a992340fd7baa7ce280fa18248
meagtan <atadenizaydin@gmail.com>
2019-06-18 17:58:29 +0300

    New toy scheme for once allocated memory

---
+++ src/test/cpp/shm_index.cpp
@@ -0,0 +1,93 @@
+// Generates changing data in different locations of shared memory
+// Simply communicate starting index and length of array in shared memory when allocating,
+// Broadcast reallocations likewise, and wait for read locks before deallocating
+// Or simply keep track of written and read segments of memory, realloc outside segments read
+// No need to wait for read locks when writing or vice versa
+
+// Producer manages memory, keeping e.g. semaphores for each segment,
+// broadcasting each allocation to readers, deallocating once all processes using segment have freed
+// Keep track of offsets of each segment in some fixed index, which are updated in realloc and checked while reading
+// Possibly broadcast or record updates too by timestamps, so reader only iterates through array once update occurs
+
+// Segment format: metadata (e.g. update, length) + array of particles each as a vector of positions and properties
+// General I/O: lookup segment offset, go to offset, check if updated, return buffer for array
+// Specific I/O: go through array of particles, process them possibly without copying
+// Perhaps think of segments as files, or at least objects or arrays allocated
+
+// Here, just add fixed indices to a lookup map, with no reallocation for now.
+
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <signal.h>
+
+#define SIZE 10000
+#define RANK 0
+
+int shmid;
+float *str;
+int cont;
+
+int update(float *str)
+{
+	static int cnt = 0;
+	// move each entry in array based on bits of cnt
+	for (int i = 0; i < SIZE/sizeof(float); ++i) {
+		str[i] += 0.02 * ((cnt & (1 << (i & ((1 << 16) - 1)))) ? 1 : -1);
+	}
+
+	++cnt;
+
+	return cont; // whether to continue
+}
+
+void detach(int signal)
+{
+	printf("\n");
+	cont = 0;
+}
+
+int main()
+{
+	key_t key = ftok("/tmp", RANK);
+	printf("key:%d\n", key);
+
+	shmid = shmget(key, SIZE, 0666|IPC_CREAT);
+
+	if (shmid < 0) {
+		printf("errno: %d\n", errno);
+		exit(1);
+	}
+
+	str = (float*) shmat(shmid,(void*)0,0);
+	// mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0);
+
+	// printf("%d\n", str);
+
+	for (int i = 0; i < SIZE/sizeof(float); ++i) {
+		// printf("%d\n", i);
+		str[i] = ((7*i) % 20 - 10) / 5.0;
+	}
+
+	std::cout << "Data written into memory: " << str[0] << std::endl;
+
+	std::cin.get();
+
+	signal(SIGINT, detach);
+	cont = 1;
+	while (update(str))
+		usleep(10000);
+
+	shmdt(str);
+	shmctl(shmid, IPC_RMID, NULL);
+
+}
+
