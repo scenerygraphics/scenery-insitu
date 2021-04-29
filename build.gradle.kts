c7963da8230764da721579e4eefc64747f893a7d
Aryaman Gupta <argupta@mpi-cbg.de>
2021-04-29 22:19:25 +0200

    place compiled classes in test jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,13 @@ dependencies {
     testImplementation("junit:junit:4.13")
     implementation("org.slf4j:slf4j-api:1.7.25")
     testImplementation("org.slf4j:slf4j-simple:1.7.25")
-    implementation("graphics.scenery:scenery:269d21d")
+//    implementation("graphics.scenery:scenery:8ed735b") {
+//        isTransitive = true
+//    }
+    api("graphics.scenery:scenery:8ed735b")
+//    {
+//        isTransitive = true
+//    }
     implementation(platform("org.lwjgl:lwjgl-bom:3.2.3"))
     implementation("net.imglib2:imglib2:5.10.0")
     implementation("net.imglib2:imglib2-roi:0.10.3")
@@ -28,6 +34,19 @@ dependencies {
 }
 
 tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+        val version = System.getProperty("java.version").substringBefore('.').toInt()
+        val default = if (version == 1) "1.8" else "$version"
+//        println("insitu is: $version")
+        kotlinOptions {
+            jvmTarget = project.properties["jvmTarget"]?.toString() ?: default
+//            println(jvmTarget)
+            freeCompilerArgs += listOf("-Xinline-classes", "-Xopt-in=kotlin.RequiresOptIn")
+        }
+        sourceCompatibility = project.properties["sourceCompatibility"]?.toString() ?: default
+//        println(sourceCompatibility)
+    }
+
     test {
         useJUnit()
     }
@@ -44,7 +63,7 @@ tasks {
 
     task<Jar>("testJar") {
         archiveClassifier.set("tests")
-        from(sourceSets.test.get().allSource)
+        from(sourceSets.test.get().output)
         dependsOn("assemble")
     }
 }
