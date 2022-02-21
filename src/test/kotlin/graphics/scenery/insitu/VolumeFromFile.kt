6a9655d96dd419ad0932b2b9bd2f319b17c3e83a
Aryaman Gupta <argupta@mpi-cbg.de>
2022-02-21 15:03:42 +0100

    generalizes raw file load method for 8 bit volume

--- src/test/kotlin/graphics/scenery/insitu/VolumeFromFile.kt
+++ src/test/kotlin/graphics/scenery/insitu/VolumeFromFile.kt
@@ -8,20 +8,26 @@ import graphics.scenery.backends.vulkan.VulkanRenderer
 import graphics.scenery.textures.Texture
 import graphics.scenery.utils.Image
 import graphics.scenery.utils.SystemHelpers
-import graphics.scenery.volumes.Colormap
-import graphics.scenery.volumes.TransferFunction
-import graphics.scenery.volumes.Volume
-import graphics.scenery.volumes.VolumeManager
+import graphics.scenery.volumes.*
+import net.imglib2.type.numeric.NumericType
+import net.imglib2.type.numeric.integer.UnsignedByteType
+import net.imglib2.type.numeric.integer.UnsignedShortType
 import net.imglib2.type.numeric.real.FloatType
 import org.joml.Quaternionf
 import org.joml.Vector3f
+import org.joml.Vector3i
 import org.lwjgl.system.MemoryUtil
 import tpietzsch.shadergen.generate.SegmentTemplate
 import tpietzsch.shadergen.generate.SegmentType
+import java.io.FileInputStream
 import java.nio.ByteBuffer
+import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
+import kotlin.streams.toList
 
 /**
  * Class to test volume rendering performance on data loaded from file
@@ -37,6 +43,73 @@ class VolumeFromFile: SceneryBase("Volume Rendering", 1832, 1016) {
     val world_abs = false
     val dataset = "Stagbeetle"
     val closeAfter = 1000000L
+
+    /**
+     * Reads raw volumetric data from a [file].
+     *
+     * Returns the new volume.
+     *
+     * Based on Volume.fromPathRaw
+     */
+    fun fromPathRaw(file: Path, is16bit: Boolean = true): BufferedVolume {
+
+        val infoFile: Path
+        val volumeFiles: List<Path>
+
+        if(Files.isDirectory(file)) {
+            volumeFiles = Files.list(file).filter { it.toString().endsWith(".raw") && Files.isRegularFile(it) && Files.isReadable(it) }.toList()
+            infoFile = file.resolve("stacks.info")
+        } else {
+            volumeFiles = listOf(file)
+            infoFile = file.resolveSibling("stacks.info")
+        }
+
+        val lines = Files.lines(infoFile).toList()
+
+        logger.debug("reading stacks.info (${lines.joinToString()}) (${lines.size} lines)")
+        val dimensions = Vector3i(lines.get(0).split(",").map { it.toInt() }.toIntArray())
+        logger.debug("setting dim to ${dimensions.x}/${dimensions.y}/${dimensions.z}")
+        logger.debug("Got ${volumeFiles.size} volumes")
+
+        val volumes = CopyOnWriteArrayList<BufferedVolume.Timepoint>()
+        volumeFiles.forEach { v ->
+            val id = v.fileName.toString()
+            val buffer: ByteBuffer by lazy {
+
+                logger.debug("Loading $id from disk")
+                val buffer = ByteArray(1024 * 1024)
+                val stream = FileInputStream(v.toFile())
+                val numBytes = if(is16bit) {
+                    2
+                } else {
+                    1
+                }
+                val imageData: ByteBuffer = MemoryUtil.memAlloc((numBytes * dimensions.x * dimensions.y * dimensions.z))
+
+                logger.debug("${v.fileName}: Allocated ${imageData.capacity()} bytes for image of $dimensions containing $numBytes per voxel")
+
+                val start = System.nanoTime()
+                var bytesRead = stream.read(buffer, 0, buffer.size)
+                while (bytesRead > -1) {
+                    imageData.put(buffer, 0, bytesRead)
+                    bytesRead = stream.read(buffer, 0, buffer.size)
+                }
+                val duration = (System.nanoTime() - start) / 10e5
+                logger.debug("Reading took $duration ms")
+
+                imageData.flip()
+                imageData
+            }
+
+            volumes.add(BufferedVolume.Timepoint(id, buffer))
+        }
+
+        return if(is16bit) {
+            Volume.fromBuffer(volumes, dimensions.x, dimensions.y, dimensions.z, UnsignedShortType(), hub)
+        } else {
+            Volume.fromBuffer(volumes, dimensions.x, dimensions.y, dimensions.z, UnsignedByteType(), hub)
+        }
+    }
 
     override fun init() {
         renderer = hub.add(Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight))
@@ -124,7 +197,7 @@ class VolumeFromFile: SceneryBase("Volume Rendering", 1832, 1016) {
         scene.addChild(shell)
         shell.visible = false
 
-        val volume = Volume.fromPathRaw(Paths.get("/home/aryaman/Datasets/Volume/$dataset"), hub)
+        val volume = fromPathRaw(Paths.get("/home/aryaman/Datasets/Volume/$dataset"))
         volume.name = "volume"
         volume.colormap = Colormap.get("hot")
         volume.spatial {
