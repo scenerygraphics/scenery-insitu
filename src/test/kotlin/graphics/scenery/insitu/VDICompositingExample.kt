d94548710c382af5c84ec1b9be425220c4bf3827
Aryaman Gupta <argupta@mpi-cbg.de>
2022-06-03 18:13:38 +0200

    changes in texture formats

--- src/test/kotlin/graphics/scenery/insitu/VDICompositingExample.kt
+++ src/test/kotlin/graphics/scenery/insitu/VDICompositingExample.kt
@@ -3,38 +3,58 @@ package graphics.scenery.insitu
 import graphics.scenery.*
 import graphics.scenery.backends.Renderer
 import graphics.scenery.backends.Shaders
+import graphics.scenery.backends.vulkan.VulkanRenderer
 import graphics.scenery.compute.ComputeMetadata
-import graphics.scenery.compute.InvocationType
 import graphics.scenery.textures.Texture
 import graphics.scenery.utils.Image
 import graphics.scenery.utils.SystemHelpers
+import graphics.scenery.volumes.vdi.VDIData
+import graphics.scenery.volumes.vdi.VDIDataIO
+import graphics.scenery.volumes.vdi.VDIMetadata
+import net.imglib2.type.numeric.real.FloatType
+import org.joml.Vector2i
 import org.joml.Vector3f
 import org.joml.Vector3i
-import org.junit.Test
 import org.lwjgl.system.MemoryUtil
-import org.scijava.ui.behaviour.ClickBehaviour
 import java.io.File
+import java.io.FileOutputStream
 import java.nio.ByteBuffer
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
+import kotlin.system.measureNanoTime
 
-class VDICompositingExample:SceneryBase("VDIComposit", 512, 512) {
+class VDICompositingExample:SceneryBase("VDIComposite", 1280, 720) {
+
+    val compute = RichNode()
+    var dataset = "DistributedStagbeetle"
+
     override fun init() {
         renderer = hub.add(SceneryElement.Renderer,
                 Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight))
 
-        val maxSupersegments = 32
+        val maxSupersegments = 20
 
         var buff = ByteArray(windowHeight*windowWidth*maxSupersegments)
         var depthBuff = ByteArray(windowHeight*windowWidth*2*maxSupersegments)
 
-        buff = File("/home/aryaman/Desktop/vdi_files/0:textureVDISetCol-2020-07-10_01.19.49.raw").readBytes()
-        depthBuff = File("/home/aryaman/Desktop/vdi_files/0:textureVDISetDepth-2020-07-10_01.19.49.raw").readBytes()
+
+        val commSize = 2
+        val rank = 1
+
+        dataset += "_${commSize}_${rank}"
+
+//        val basePath = "/home/aryaman/Repositories/DistributedVis/cmake-build-debug/"
+//        val basePath = "/home/aryaman/Repositories/scenery-insitu/"
+        val basePath = "/home/aryaman/TestingData/"
+
+        buff = File(basePath + "${dataset}SetOfVDI4_ndc_col").readBytes()
+        depthBuff = File(basePath + "${dataset}SetOfVDI4_ndc_depth").readBytes()
 
         var colBuffer: ByteBuffer
         var depthBuffer: ByteBuffer
 //        colBuffer = ByteBuffer.wrap(buff)
 //        depthBuffer = ByteBuffer.wrap(depthBuff)
-        colBuffer = MemoryUtil.memCalloc(windowHeight * windowWidth * maxSupersegments * 4)
+        colBuffer = MemoryUtil.memCalloc(windowHeight * windowWidth * maxSupersegments * 4 * 4)
         depthBuffer = MemoryUtil.memCalloc(windowHeight * windowWidth * maxSupersegments * 4 * 2)
 
         logger.info("Length of color buffer is ${buff.size} and associated bytebuffer capacity is ${colBuffer.capacity()} it has remaining: ${colBuffer.remaining()}")
@@ -43,46 +63,117 @@ class VDICompositingExample:SceneryBase("VDIComposit", 512, 512) {
         depthBuffer.put(depthBuff).flip()
 
 
-        val maxOutputSupersegments = 50
-        val commSize = 2;
-        val compute = Box()
+        val maxOutputSupersegments = 40
 
         compute.name = "compositor node"
 
         compute.setMaterial(ShaderMaterial(Shaders.ShadersFromFiles(arrayOf("VDICompositor.comp"), this@VDICompositingExample::class.java)))
-        val outputColours = MemoryUtil.memCalloc(maxOutputSupersegments*windowHeight*windowWidth*4 / commSize)
-        val compositedVDIColor = Texture.fromImage(Image(outputColours, maxOutputSupersegments, windowHeight,  windowWidth/commSize), usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
+        val outputColours = MemoryUtil.memCalloc(maxOutputSupersegments*windowHeight*windowWidth*4*4 / commSize)
+        val compositedVDIColor = Texture.fromImage(Image(outputColours, maxOutputSupersegments, windowHeight,  windowWidth/commSize), channels = 4, usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture),
+            type = FloatType(), mipmap = false, normalized = false, minFilter = Texture.FilteringMode.NearestNeighbour, maxFilter = Texture.FilteringMode.NearestNeighbour)
+
         compute.material().textures["CompositedVDIColor"] = compositedVDIColor
 
-        val outputDepths = MemoryUtil.memCalloc(2*maxOutputSupersegments*windowHeight*windowWidth*4 / commSize)
-        val compositedVDIDepth = Texture.fromImage(Image(outputDepths, 2*maxOutputSupersegments, windowHeight,  windowWidth/commSize), usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
+        val outputDepths = MemoryUtil.memCalloc(maxOutputSupersegments*windowHeight*windowWidth*4*2 / commSize)
+        val compositedVDIDepth = Texture.fromImage(Image(outputDepths, 2 * maxOutputSupersegments, windowHeight,  windowWidth/commSize), channels = 1, usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture)
+            , type = FloatType(), mipmap = false, normalized = false, minFilter = Texture.FilteringMode.NearestNeighbour, maxFilter = Texture.FilteringMode.NearestNeighbour)
+
         compute.material().textures["CompositedVDIDepth"] = compositedVDIDepth
 
         compute.metadata["ComputeMetadata"] = ComputeMetadata(
-                workSizes = Vector3i(windowHeight, windowWidth/commSize, 1)
+                workSizes = Vector3i(windowWidth/commSize, windowHeight, 1)
         )
 
-        compute.material().textures["VDIsColor"] = Texture(Vector3i(maxSupersegments, windowHeight, windowWidth), 4, contents = colBuffer, usageType = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
-        compute.material().textures["VDIsDepth"] = Texture(Vector3i(maxSupersegments*2, windowHeight, windowWidth), 4, contents = depthBuffer, usageType = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
+        compute.material().textures["VDIsColor"] = Texture(Vector3i(maxSupersegments, windowHeight, windowWidth), 4, contents = colBuffer, usageType = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture),
+            type = FloatType(), mipmap = false, normalized = false, minFilter = Texture.FilteringMode.NearestNeighbour, maxFilter = Texture.FilteringMode.NearestNeighbour)
+
+        compute.material().textures["VDIsDepth"] = Texture(Vector3i(2*maxSupersegments, windowHeight, windowWidth),  channels = 1, contents = depthBuffer, usageType = hashSetOf(
+            Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture), type = FloatType(), mipmap = false, normalized = false, minFilter = Texture.FilteringMode.NearestNeighbour, maxFilter = Texture.FilteringMode.NearestNeighbour)
+
         logger.info("Updated the ip textured")
 
         compute.visible = true
         scene.addChild(compute)
 
         val light = PointLight(radius = 15.0f)
-        light.position = Vector3f(0.0f, 0.0f, 2.0f)
+        light.spatial().position = Vector3f(0.0f, 0.0f, 2.0f)
         light.intensity = 5.0f
         light.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
         scene.addChild(light)
 
         val cam: Camera = DetachedHeadCamera()
         with(cam) {
-            position = Vector3f(0.0f, 0.0f, 5.0f)
+            spatial().position = Vector3f(0.0f, 0.0f, 5.0f)
             perspectiveCamera(50.0f, 512, 512)
 
             scene.addChild(this)
         }
 
+        thread {
+            storeCompositedVDIs()
+        }
+
+    }
+
+    fun storeCompositedVDIs() {
+        var compositedVDIDepthBuffer: ByteBuffer? = null
+        var compositedVDIColorBuffer: ByteBuffer?
+
+        while(renderer?.firstImageReady == false) {
+//        while(renderer?.firstImageReady == false || volumeManager.shaderProperties.isEmpty()) {
+            Thread.sleep(50)
+        }
+
+        val compositedVDIColor = compute.material().textures["CompositedVDIColor"]!!
+        val colorCounter = AtomicInteger(0)
+
+        (renderer as? VulkanRenderer)?.persistentTextureRequests?.add (compositedVDIColor to colorCounter)
+
+        val depthCounter = AtomicInteger(0)
+        var compositedVDIDepth: Texture? = null
+
+        if(separateDepth) {
+            compositedVDIDepth = compute.material().textures["CompositedVDIDepth"]!!
+            (renderer as? VulkanRenderer)?.persistentTextureRequests?.add (compositedVDIDepth to depthCounter)
+        }
+
+        var prevAtomic = colorCounter.get()
+
+        var cnt = 0
+
+        val tGeneration = Timer(0, 0)
+
+        while (true) {
+            tGeneration.start = System.nanoTime()
+            while(colorCounter.get() == prevAtomic) {
+                Thread.sleep(5)
+            }
+            prevAtomic = colorCounter.get()
+            compositedVDIColorBuffer = compositedVDIColor.contents
+            if(separateDepth) {
+                compositedVDIDepthBuffer = compositedVDIDepth!!.contents
+            }
+
+            tGeneration.end = System.nanoTime()
+
+            val timeTaken = tGeneration.end - tGeneration.start
+
+            logger.info("Time taken for generation (only correct if VDIs were not being written to disk): ${timeTaken}")
+
+            if(cnt < 20) {
+
+                var fileName = ""
+                fileName = "${dataset}CompositedVDI${cnt}_ndc"
+                if(separateDepth) {
+                    SystemHelpers.dumpToFile(compositedVDIColorBuffer!!, "/home/aryaman/TestingData/${fileName}_col")
+                    SystemHelpers.dumpToFile(compositedVDIDepthBuffer!!, "/home/aryaman/TestingData/${fileName}_depth")
+                } else {
+                    SystemHelpers.dumpToFile(compositedVDIColorBuffer!!, fileName)
+                }
+                logger.info("Wrote VDI $cnt")
+            }
+            cnt++
+        }
     }
 
     override fun inputSetup() {
@@ -123,8 +214,10 @@ class VDICompositingExample:SceneryBase("VDIComposit", 512, 512) {
         inputHandler?.addKeyBinding("save_texture", "E")
     }
 
-    @Test
-    override fun main() {
-        super.main()
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            VDICompositingExample().main()
+        }
     }
 }
